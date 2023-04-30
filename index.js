7ca6a18878dcf1c088b00e005cb83de7f95c8d6f
geovany <glopezvan@gmail.com>
2023-04-29 20:58:01 -0500

    mejoras a la calculadora
    se mejora la logica y se trabaja con datos ingresados por el usuario.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+/*let num1 = (document.getElementById("num1").textContent = Math.floor(
+  Math.random() * (100 - 0) + 0
+));
+let num2 = (document.getElementById("num2").textContent = Math.floor(
+  Math.random() * (100 - 0) + 0
+));*/
+let num1 = document.getElementById("num1").textContent = parseInt(prompt('Ingrese un numero'));
+let num2 = document.getElementById("num2").textContent = parseInt(prompt('Ingrese otro numero'));
 
-let num1 = document.getElementById('num1').textContent = 50;
-let num2 = document.getElementById('num2').textContent = 10;
 
-function add(){
-    let add = num1+num2;
-    document.getElementById('total').textContent = add; 
+
+function add() {
+  let add = num1 + num2;
+  document.getElementById("total").textContent = add;
 }
-function subtract(){
-    let subtract = num1-num2;
-    document.getElementById('total').textContent = subtract;
+function subtract() {
+  let subtract = num1 - num2;
+  document.getElementById("total").textContent = subtract;
 }
-function multiply(){
-    let multiply = num1 * num2;
-    document.getElementById('total').textContent = multiply;
+function multiply() {
+  let multiply = num1 * num2;
+  document.getElementById("total").textContent = multiply;
 }
-function divide(){
-    let divide = num1 / num2;
-    document.getElementById('total').textContent = divide;
+function divide() {
+  let divide = num1 / num2;
+  document.getElementById("total").textContent = divide;
 }
